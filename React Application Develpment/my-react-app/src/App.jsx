7b9871ae0c7df2b56b559b0b1096e6d3fb32238b
Jayratnashivasharan <jayratnashivasharan8@gmail.com>
2026-08-21 12:20:57 +0530

    added

--- React Application Develpment/my-react-app/src/App.jsx
+++ React Application Develpment/my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Student from './student.jsx'
+import Student1 from './student1.jsx'
 import Welcome from './welcome.jsx'
 
 function App() {
@@ -7,9 +8,17 @@ function App() {
     <main className="page-shell">
       <section className="form-card">
         <Welcome />
-        <Student name="Honey" age="22" course="MERN" />
-        <Student name="Jay" age="23" course="MERN" />
 
+        <Student name="Honey" age="22" course="MERN" />
+        <hr />
+
+        <Student name="Jay" age="23" course="MERN" />
+        <hr />
+
+        <Student1 name="Jayu" age="22" course="Python" />
+        <hr />
+
+        <Student1 name="Bunny" age="21" course="JAVA" />
       </section>
     </main>
   )
---
+++ React Application Develpment/my-react-app/src/student1.jsx
@@ -0,0 +1,11 @@
+function Student1(props) {
+  return (
+    <>
+      <h2>Student Name: {props.name}</h2>
+      <h2>Student Age: {props.age}</h2>
+      <h2>Student Course: {props.course}</h2>
+    </>
+  );
+}
+
+export default Student1;
